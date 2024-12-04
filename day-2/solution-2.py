3bc08aca2512b85b956c05f24c92624147d018c4
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-12-04 20:54:45 +0500

    Day #2

---
+++ day-2/solution-2.py
@@ -0,0 +1,43 @@
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+
+reports  = []
+
+for line in lines: 
+    numbers = line.split()
+    levels = []
+    for num in numbers:
+        levels.append(int(num))
+    reports.append(levels)
+
+
+def non_decreasing(L):
+    return all(x<=y for x, y in zip(L, L[1:]))
+def non_increasing(L):
+    return all(x>=y for x, y in zip(L, L[1:]))
+def monotonic(L):
+    return non_decreasing(L) or non_increasing(L)
+
+def is_safe(level):
+    if (not monotonic(level)):
+        return False
+    
+    for i in range(1, len(level)):
+        dis = abs(level[i] - level[i - 1])
+        if (dis < 1 or dis > 3):
+            return False
+    
+    return True
+    
+res = 0
+for level in reports:
+    if (is_safe(level)):
+        res += 1
+        continue
+        
+    for i in range(len(level)):
+        if (is_safe(level=level[:i] + level[i + 1:])):
+            res += 1
+            break
+
+print(res)
